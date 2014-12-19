939a140ba63c0127992b4b2932581398a62dcfe0
hongzhu <hong.zhu.van@gmail.com>
2014-12-18 16:39:22 -0800

    fall back to apache2.2

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
 # install apache http24
-wget -q http://repos.fedorapeople.org/repos/jkaluza/httpd24/epel-httpd24.repo
-sudo mv epel-httpd24.repo /etc/yum.repos.d
-sudo yum install -yq httpd24.x86_64
-sudo chkconfig --add httpd24-httpd
-sudo chkconfig httpd24-httpd on
-sudo service httpd24-httpd start
+sudo yum install -y httpd httpd-tools 
+sudo chkconfig --add httpd
+sudo chkconfig httpd on
+sudo service httpd start
 
 # install php55
 sudo rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
-sudo yum install -yq php55w php55w-opcache
+sudo yum install -y php55w php55w-opcache php55w-fpm php55w-gd
 
 # install mysql56
 wget http://repo.mysql.com/mysql-community-release-el6-5.noarch.rpm
 sudo rpm -ivh mysql-community-release-el6-5.noarch.rpm
-sudo yum -yq install mysql-server
+sudo yum -y install mysql-server
 sudo chkconfig --add mysqld
 sudo chkconfig mysqld on
 sudo service mysqld start
